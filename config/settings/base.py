d5f433dd83c38477c44ad818d6429dc826bf49b3
Samar Hassan <samar.hassan@arbisoft.com>
2023-02-16 15:24:16 +0500

    chore :recycle: add comments app

--- config/settings/base.py
+++ config/settings/base.py
@@ -80,7 +80,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "chattin.users",
-    # Your stuff: custom apps go here
+    "chattin.comments",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
