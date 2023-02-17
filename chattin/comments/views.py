13a270ccb09fd2f67026497fcdfe78992e9e6d1a
Samar Hassan <samar.hassan@arbisoft.com>
2023-02-17 20:16:12 +0500

    feat :star: select and prefetch related items in comments

--- chattin/comments/views.py
+++ chattin/comments/views.py
@@ -1,4 +1,5 @@
 from comments.models import Comment
+from django.db.models import Count, Prefetch
 from django.views import generic
 
 
@@ -7,7 +8,31 @@ class ArticleDetail(generic.TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["comments"] = Comment.objects.filter(
-            page_id=kwargs["id"], level=0
-        ).order_by("-date_created")
+        context["comments"] = (
+            Comment.objects.filter(page_id=kwargs["id"], level=0)
+            .annotate(like_count=Count("likes"), dislike_count=Count("dislikes"))
+            .select_related("user")
+            .prefetch_related(
+                Prefetch(
+                    "replies",
+                    queryset=Comment.objects.filter(level=1)
+                    .annotate(
+                        like_count=Count("likes"), dislike_count=Count("dislikes")
+                    )
+                    .select_related("user")
+                    .prefetch_related(
+                        Prefetch(
+                            "replies",
+                            queryset=Comment.objects.filter(level=2)
+                            .annotate(
+                                like_count=Count("likes"),
+                                dislike_count=Count("dislikes"),
+                            )
+                            .select_related("user"),
+                        ),
+                    ),
+                ),
+            )
+            .order_by("-date_created")
+        )
         return context
