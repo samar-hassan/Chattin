c6c135c8817113546d441ec22dceba840a903b65
Samar Hassan <samar.hassan@arbisoft.com>
2023-02-24 00:28:47 +0500

    feat :star: api to like and dislike comment

--- chattin/comments/urls.py
+++ chattin/comments/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from chattin.comments.views import (
     ArticleDetail, CommentCreateView, CommentUpdateView, CommentDestroyView,
+    LikeView, LikeDestroyView, DislikeView, DislikeDestroyView,
 )
 
 app_name = "comments"
@@ -10,4 +11,8 @@ urlpatterns = [
     path("api/comment/add", CommentCreateView.as_view(), name="comment-add"),
     path("api/comment/<int:pk>", CommentUpdateView.as_view(), name="comment-update"),
     path("api/comment/delete/<int:pk>", CommentDestroyView.as_view(), name="comment-delete"),
+    path("api/comment/like/<int:pk>", LikeView.as_view(), name="like"),
+    path("api/comment/remove-like/<int:pk>", LikeDestroyView.as_view(), name="like-destroy"),
+    path("api/comment/dislike/<int:pk>", DislikeView.as_view(), name="dislike"),
+    path("api/comment/remove-dislike/<int:pk>", DislikeDestroyView.as_view(), name="dislike-destroy"),
 ]
--- chattin/comments/views.py
+++ chattin/comments/views.py
@@ -1,10 +1,12 @@
 from comments.models import Comment
 from django.db.models import Count, Prefetch
 from django.views import generic
+from django.shortcuts import get_object_or_404
 
 from rest_framework import status
 from rest_framework.generics import DestroyAPIView, UpdateAPIView, CreateAPIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from chattin.permissions import IsOwner
 from comments.serializers import (
@@ -54,11 +56,59 @@ class CommentCreateView(CreateAPIView):
 
 
 class CommentUpdateView(UpdateAPIView):
+    """API view to update comment."""
     permission_classes = (IsOwner, )
     serializer_class = UpdateCommentSerializer
     queryset = Comment.objects.all()
 
 
 class CommentDestroyView(DestroyAPIView):
+    """API view to delete comment."""
     permission_classes = (IsOwner, )
     queryset = Comment.objects.all()
+
+
+class LikeView(UpdateAPIView):
+    """API view to like comment."""
+    permission_classes = (IsAuthenticated, )
+    queryset = Comment.objects.all()
+
+    def update(self, *args,  **kwargs):
+        comment = get_object_or_404(Comment, pk=kwargs['pk'])
+        if not self.request.user.id in list(comment.likes.values_list('id', flat=True)):
+            comment.likes.add(self.request.user)
+        return Response("Liked Comment", status=status.HTTP_200_OK)
+
+
+class LikeDestroyView(DestroyAPIView):
+    """API view to remove like."""
+    permission_classes = (IsAuthenticated,)
+
+    def delete(self, *args,  **kwargs):
+        comment = get_object_or_404(Comment, pk=kwargs['pk'])
+        if self.request.user.id in list(comment.likes.values_list('id', flat=True)):
+            comment.likes.remove(self.request.user)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class DislikeView(UpdateAPIView):
+    """API view to dislike comment."""
+    permission_classes = (IsAuthenticated, )
+    queryset = Comment.objects.all()
+
+    def update(self, *args,  **kwargs):
+        comment = get_object_or_404(Comment, pk=kwargs['pk'])
+        if not self.request.user.id in list(comment.dislikes.values_list('id', flat=True)):
+            comment.dislikes.add(self.request.user)
+        return Response("Disliked Comment", status=status.HTTP_200_OK)
+
+
+class DislikeDestroyView(DestroyAPIView):
+    """API view to remove dislike."""
+    permission_classes = (IsAuthenticated,)
+
+    def delete(self, *args,  **kwargs):
+        comment = get_object_or_404(Comment, pk=kwargs['pk'])
+        if self.request.user.id in list(comment.dislikes.values_list('id', flat=True)):
+            comment.dislikes.remove(self.request.user)
+        return Response(status=status.HTTP_204_NO_CONTENT)
