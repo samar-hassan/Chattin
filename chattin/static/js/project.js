f2c5a9edd9fc8ae8df5e72dfe700f072d3ce24b0
Samar Hassan <samar.hassan@arbisoft.com>
2023-02-23 20:11:45 +0500

    feat :star: add edit delete options in dropdown

--- chattin/static/js/project.js
+++ chattin/static/js/project.js
@@ -15,18 +15,27 @@ $(document).ready(function() {
         getComment(text, level=1, name=this.user, avatar=this.avatarName) {
             return `<div class="mb-3 comment row level-${level}" data-level="${level}">
                 <div class="col-1 avatar">${avatar}</div>
-                <div class="col-10" style="max-width: 90%;">
-                    <p class="m-0 p-0 name">${name}</p>
+                <div class="col-10 title-row">
+                    <div class="d-flex justify-content-between">
+                        <p class="m-0 p-0 name">${name}</p>
+                        <div class="dropdown">
+                            <img class="ellipse" src="/static/images/ellipsis.svg" id="dropdownMenuButton2" data-bs-toggle="dropdown">
+                            <ul class="dropdown-menu dropdown-menu" aria-labelledby="dropdownMenuButton2">
+                            <li class="dropdown-item">Edit</li>
+                            <li class="dropdown-item">Delete</li>
+                            </ul>
+                        </div>
+                    </div>
                     <p class="m-0 p-0 text">
                         ${text}
                     </p>
                     <div class="d-flex">
                         <div>
-                            <img class="thumbs" src="/static/images/thumbs-up.svg" alt="">
+                            <img class="thumbs" src="/static/images/thumbs-up.svg">
                             <span class="likes text">0</span>
                         </div>
                         <div class="mx-3">
-                            <img class="thumbs" src="/static/images/thumbs-down.svg" alt="">
+                            <img class="thumbs" src="/static/images/thumbs-down.svg">
                             <span class="dislikes text">0</span>
                         </div>
                         ${level <=1? '<span class="reply">reply</span>': ''}
@@ -56,7 +65,7 @@ $(document).ready(function() {
             const replyTo = $commentDiv.data('comment');
             const inputId  = `input-${replyTo}-${level}-${Math.ceil(Math.random() * 10000)}`;
             const replySection = `<div class="replies"><div class="section-level">
-                <div class="mb-3 row level-${level + 1}">
+                <div class="mb-3 row level-${level + 1} w-75">
                     <div class="col-1 avatar">${this.avatarName}</div>
                     <div class="col-10">
                         <input type="text" class="form-control" data-level="${level + 1}" data-reply-to="${replyTo}"
